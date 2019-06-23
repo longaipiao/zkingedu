6c39b1447a1a83db74551dbef4630ea2f2c6f0f5
root <longhuibiao@qq.com>
2019-06-23 23:21:30 +0800

    L

--- zkingedu/src/main/java/com/zking/zkingedu/common/service/PostService.java
+++ zkingedu/src/main/java/com/zking/zkingedu/common/service/PostService.java
@@ -1,7 +1,26 @@
 package com.zking.zkingedu.common.service;
 
+import com.zking.zkingedu.common.model.Pcata;
+import com.zking.zkingedu.common.model.Post;
+
+import java.util.List;
+
 /**
  * 帖子接口
  */
+
 public interface PostService {
+
+    /**
+     * 增加贴子
+     * @param post
+     * @return
+     */
+    public int addPost(Post post);
+
+    /**
+     * 获得所有的分类帖子
+     */
+    List<Pcata> getpctae();
+
 }
--- zkingedu/src/main/java/com/zking/zkingedu/common/service/impl/PostServiceImpl.java
+++ zkingedu/src/main/java/com/zking/zkingedu/common/service/impl/PostServiceImpl.java
@@ -1,11 +1,37 @@
 package com.zking.zkingedu.common.service.impl;
 
+import com.zking.zkingedu.common.dao.PostDao;
+import com.zking.zkingedu.common.model.Pcata;
+import com.zking.zkingedu.common.model.Post;
 import com.zking.zkingedu.common.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 帖子接口  实现层
  */
 @Service("postService")
 public class PostServiceImpl implements PostService {
+
+    @Resource
+    private PostDao postDao;
+
+    /**
+     * 增加贴子
+     * @param post
+     * @return
+     */
+    @Override
+    public int addPost(Post post) {
+
+        return postDao.addPost(post);
+    }
+
+    @Override
+    public List<Pcata> getpctae() {
+        return postDao.getpctae();
+    }
 }
