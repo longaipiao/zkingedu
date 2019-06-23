0fdd044040a44c0a9d7922683e019aff9d4462b7
longaipiao <longhuibiao@qq.com>
2019-06-23 15:32:50 +0800

    阿飘提交

--- zkingedu/src/main/java/com/zking/zkingedu/common/service/ToolService.java
+++ zkingedu/src/main/java/com/zking/zkingedu/common/service/ToolService.java
@@ -1,7 +1,18 @@
 package com.zking.zkingedu.common.service;
 
+import com.github.pagehelper.PageInfo;
+import com.zking.zkingedu.common.model.Tool;
+
+import java.util.List;
+
 /**
  * 开发者工具接口
  */
 public interface ToolService {
+    /**
+     * 查询所有的工具信息
+     * @param
+     * @return
+     */
+    public PageInfo<Tool> findAll(Integer page, Integer pageSize);
 }
--- zkingedu/src/main/java/com/zking/zkingedu/common/service/impl/ToolServiceImpl.java
+++ zkingedu/src/main/java/com/zking/zkingedu/common/service/impl/ToolServiceImpl.java
@@ -1,11 +1,29 @@
 package com.zking.zkingedu.common.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zking.zkingedu.common.dao.ToolDao;
+import com.zking.zkingedu.common.model.Tool;
 import com.zking.zkingedu.common.service.ToolService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 开发者工具接口
  */
 @Service("toolService")
 public class ToolServiceImpl implements ToolService {
+
+    @Resource
+    private ToolDao toolDao;
+
+    @Override
+    public PageInfo<Tool> findAll(Integer page, Integer pageSize) {
+        PageHelper.startPage(page, pageSize);
+        List<Tool> tools = toolDao.findAll();
+        return new PageInfo<>(tools);
+    }
 }
